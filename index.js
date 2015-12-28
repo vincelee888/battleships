5005689ec50e3a7b3d5d5113926dfb93c6429d4e
vince (polyhat) <vslee888@gmail.com>
2015-12-28 21:19:54 +0000

    stubbed endpoints

--- index.js
+++ index.js
@@ -3,8 +3,40 @@ var app = express()
 
 app.set('port', (process.env.PORT || 5000))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
+app.get('/', function(req, res) {
+  res.send('I doubt Rihanna\'s nautical credentials.')
+})
+
+app.post('/start', function(req, res) {
+	res.sendStatus(200)
+})
+
+app.get('/place', function(req, res) {
+	res.sendStatus(200)
+})
+
+app.post('/place', function(req, res) {
+	res.sendStatus(200)
+})
+
+app.get('/move', function(req, res) {
+	res.sendStatus(200)
+})
+
+app.post('/hit', function(req, res) {
+	res.sendStatus(200)
+})
+
+app.post('/miss', function(req, res) {
+	res.sendStatus(200)
+})
+
+app.post('/scan', function(req, res) {
+	res.sendStatus(200)
+})
+
+app.post('/hit_mine', function(req, res) {
+	res.sendStatus(200)
 })
 
 app.listen(app.get('port'), function() {
