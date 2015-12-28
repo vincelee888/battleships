2485046de427a5c786bba09335bd10aeb5980d22
vince (polyhat) <vslee888@gmail.com>
2015-12-28 21:32:52 +0000

    added logging of req.body

--- index.js
+++ index.js
@@ -9,49 +9,49 @@ app.get('/', function(req, res) {
 
 app.post('/start', function(req, res) {
 	console.log('post /start')
-	console.log(req)
+	console.log(req.body)
 	res.sendStatus(200)
 })
 
 app.get('/place', function(req, res) {
 	console.log('get /place')
-	console.log(req)
+	console.log(req.body)
 	res.sendStatus(200)
 })
 
 app.post('/place', function(req, res) {
 	console.log('post /place')
-	console.log(req)
+	console.log(req.body)
 	res.sendStatus(200)
 })
 
 app.get('/move', function(req, res) {
 	console.log('get /move')
-	console.log(req)
+	console.log(req.body)
 	res.sendStatus(200)
 })
 
 app.post('/hit', function(req, res) {
 	console.log('post /hit')
-	console.log(req)
+	console.log(req.body)
 	res.sendStatus(200)
 })
 
 app.post('/miss', function(req, res) {
 	console.log('post /miss')
-	console.log(req)
+	console.log(req.body)
 	res.sendStatus(200)
 })
 
 app.post('/scan', function(req, res) {
 	console.log('post /scan')
-	console.log(req)
+	console.log(req.body)
 	res.sendStatus(200)
 })
 
 app.post('/hit_mine', function(req, res) {
 	console.log('post /hit_mine')
-	console.log(req)
+	console.log(req.body)
 	res.sendStatus(200)
 })
 
