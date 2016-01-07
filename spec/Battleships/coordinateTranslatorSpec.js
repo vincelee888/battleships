63561c58bc6196a1bf5785a8bfc04ca427e32328
vince (polyhat) <vslee888@gmail.com>
2016-01-07 21:36:34 +0000

    fix translator

--- spec/Battleships/coordinateTranslatorSpec.js
+++ spec/Battleships/coordinateTranslatorSpec.js
@@ -1,17 +1,12 @@
 describe('CoordinateTranslator', function() {
-	var CoordinateTranslator = require('../../lib/Battleships/CoordinateTranslator')	
-	var translator
+	var translator = require('../../lib/Battleships/CoordinateTranslator')	
 
-	beforeEach(function() {	
-		translator = new CoordinateTranslator()
-	})
-
-	it('should translate columns as character in its position in the alphabet', function() {
+	it('should translator columns as character in its position in the alphabet', function() {
 		expect(translator.translate("A1").columns).toEqual(1)
 		expect(translator.translate("Z1").columns).toEqual(26)
 	})
 
-	it('should translate rows as the remaining numerical value', function() {
+	it('should translator rows as the remaining numerical value', function() {
 		expect(translator.translate("A1").rows).toEqual(1)
 		expect(translator.translate("A26").rows).toEqual(26)
 	})
