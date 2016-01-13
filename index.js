edec3c86855a8ab23959fb219ea7920b122e7fb6
vince (polyhat) <vslee888@gmail.com>
2016-01-13 20:09:07 +0000

    added logs

--- index.js
+++ index.js
@@ -12,38 +12,58 @@ app.get('/', function(req, res) {
 })
 
 app.post('/start', function(req, res) {
+	console.log('---POST: START---')
+	console.log(req)
 	res.sendStatus(200)
 })
 
 app.get('/place', function(req, res) {
+	console.log('---GET: PLACE---')
+	console.log(req)
 	res.sendStatus(200)
 })
 
 app.post('/place', function(req, res) {
+	console.log('---POST: PLACE---')
+	console.log(req)
 	res.sendStatus(200)
 })
 
 app.get('/move', function(req, res) {
+	console.log('---GET: MOVE---')
+	console.log(req)
+
+	var move = game.move()
+
+	console.log(move)
     res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(game.move()));
+    res.send(JSON.stringify(move));
 })
 
 app.post('/hit', function(req, res) {
+	console.log('---POST: HIT---')
+	console.log(req)
 	game.registerHit(req)
 	res.sendStatus(200)
 })
 
 app.post('/miss', function(req, res) {
+	console.log('---POST: MISS---')
+	console.log(req)
 	game.registerMiss(req)
 	res.sendStatus(200)
 })
 
 app.post('/hit_mine', function(req, res) {
+	console.log('---POST: HIT_MINE---')
+	console.log(req)
 	game.registerMine(req)
 	res.sendStatus(200)
 })
 
 app.post('/scan', function(req, res) {
+	console.log('---POST: SCAN---')
+	console.log(req)
 	res.sendStatus(200)
 })
 
